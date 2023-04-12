f4f27f3d0090b90d170ea4089b4a9ec8a4ccd671
Brandon Tor <brandon.tor@hotmail.com>
2023-04-11 23:52:22 -0400

    fixing deployment issues

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,21 +1,18 @@
-import { type ReactNode, FC } from "react";
-import { useUser, SignInButton, SignOutButton} from "@clerk/nextjs";
+import { type ReactNode } from "react";
+import { useUser, SignInButton, SignOutButton } from "@clerk/nextjs";
 import { NavBar } from "~/components/NavBar";
 import { Button } from "../components/ui/Button";
 import { ButtonLoading } from "~/components/ui/ButtonLoading";
 import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
-import type {UserResource} from '@clerk/types'
+import type { UserResource } from "@clerk/types";
 
-
-
-interface Props  {
+interface Props {
   children: ReactNode;
-  user?: UserResource
-};
+  user?: UserResource;
+}
 
-const Layout: React.FC<Props>  = (props) => {
-  const { isSignedIn, isLoaded, user} = useUser();
-
+const Layout = (props: Props) => {
+  const { isSignedIn, isLoaded, user } = useUser();
 
   return (
     <>
@@ -29,7 +26,9 @@ const Layout: React.FC<Props>  = (props) => {
             </Button>
           ))}
 
-        {isSignedIn && <AppContainer user={user}>{props.children}</AppContainer>}
+        {isSignedIn && (
+          <AppContainer user={user}>{props.children}</AppContainer>
+        )}
       </main>
     </>
   );
@@ -38,16 +37,19 @@ const Layout: React.FC<Props>  = (props) => {
 export default Layout;
 
 const AppContainer = ({ children, user }: Props) => {
-  
-  if(!user) return <div>Something went Wrong</div>
+  if (!user) return <div>Something went Wrong</div>;
 
   return (
     <div className="  flex h-5/6  w-11/12 rounded-lg bg-slate-100 shadow-sm shadow-slate-100">
-      <div className=" relative h-full w-1/6 rounded-tl-lg rounded-bl-lg border-r-2 border-slate-500 bg-slate-100">
-        <div className="mt-4 ml-2">
+      <div className=" relative h-full w-1/6 rounded-bl-lg rounded-tl-lg border-r-2 border-slate-500 bg-slate-100">
+        <div className="ml-2 mt-4">
           <Avatar>
             <AvatarImage src={`${user.profileImageUrl}`} />
-            <AvatarFallback>{`${user.fullName}'s Profile Picture`}</AvatarFallback>
+            <AvatarFallback>
+              {user.fullName
+                ? `${user.fullName}'s Profile Picture`
+                : "Profile Picture"}
+            </AvatarFallback>
           </Avatar>
         </div>
         <NavBar></NavBar>
