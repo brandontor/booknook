cd359e6db450224a48c7e0dfb83f0fe0e913f6fe
Brandon Tor <brandon.tor@hotmail.com>
2023-12-21 00:13:32 -0500

    command bar added and moved to header

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -4,7 +4,21 @@ import React from "react"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { useDebouncedCallback } from "use-debounce"
 
+import { cn } from "@/lib/utils"
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command"
 import { Input } from "@/components/ui/input"
+
+import { Button } from "./ui/button"
 
 type Props = {}
 
@@ -12,6 +26,18 @@ function SearchBar({}: Props) {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
+  const [open, setOpen] = React.useState(false)
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setOpen((open) => !open)
+      }
+    }
+    document.addEventListener("keydown", down)
+    return () => document.removeEventListener("keydown", down)
+  }, [])
 
   const searchHandler = useDebouncedCallback(async (book: string) => {
     const params = new URLSearchParams(searchParams)
@@ -30,15 +56,43 @@ function SearchBar({}: Props) {
   }, 300)
 
   return (
-    <div>
-      <Input
-        type="text"
-        onChange={(e) => {
-          searchHandler(e.target.value)
-        }}
-        defaultValue={searchParams.get("q")?.toString()}
-      ></Input>
-    </div>
+    <>
+      <Button
+        variant="outline"
+        className={cn(
+          "relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64"
+        )}
+        onClick={() => setOpen(true)}
+      >
+        <span className="hidden lg:inline-flex">Search documentation...</span>
+        <span className="inline-flex lg:hidden">Search...</span>
+        <kbd className="pointer-events-none absolute right-1.5 top-2 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Search">
+            <CommandItem
+              onSelect={() => {
+                console.log("TITLE")
+              }}
+            >
+              Search by Title
+            </CommandItem>
+            <CommandItem
+              onSelect={() => {
+                console.log("Author")
+              }}
+            >
+              Search by Author
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
   )
 }
 
